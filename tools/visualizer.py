7fece4a0b62afd4d68aae42e19a3c7117609355a
Jules Talloen <jules.talloen@ml6.eu>
2020-09-30 11:00:11 +0200

    WIP

--- tools/visualizer.py
+++ tools/visualizer.py
@@ -1,6 +1,8 @@
+"""This module contains a Visualizer class to visualize re-identification query
+images, gallery images and results."""
+
 import os
 import os.path as osp
-import errno
 
 import numpy as np
 import cv2 as cv
@@ -11,91 +13,6 @@ Q_SPACING = 90
 BW = 5  # border width
 GREEN = (0, 255, 0)
 RED = (0, 0, 255)
-
-
-def mkdir_if_missing(dirname):
-    """Creates dirname if it is missing."""
-    if not osp.exists(dirname):
-        try:
-            os.makedirs(dirname)
-        except OSError as e:
-            if e.errno != errno.EEXIST:
-                raise
-
-
-def visualize_ranked_results(
-        distmat, g_dataset, q_dataset, width=128, height=256,
-        save_dir='', topk=10
-):
-    num_q, num_g = distmat.shape
-    mkdir_if_missing(save_dir)
-
-    print('# query: {}\n# gallery {}'.format(num_q, num_g))
-    print('Visualizing top-{} ranks ...'.format(topk))
-
-    query, gallery = q_dataset, g_dataset
-    assert num_q == len(query)
-    assert num_g == len(gallery)
-
-    indices = np.argsort(distmat, axis=1)
-
-    for q_idx in range(num_q):
-        qimg_paths, qpid, qcamid = query[q_idx]
-        qimg_path = qimg_paths[0]
-        qimg_path_name = qimg_path[0] if isinstance(
-            qimg_path, (tuple, list)
-        ) else qimg_path
-
-        qimg = cv.imread(qimg_path)
-        qimg = cv.resize(qimg, (width, height))
-        qimg = cv.copyMakeBorder(
-            qimg, BW, BW, BW, BW, cv.BORDER_CONSTANT, value=(0, 0, 0)
-        )
-        # resize twice to ensure that the border width is consistent
-        qimg = cv.resize(qimg, (width, height))
-        num_cols = topk + 1
-        grid_img = 255 * np.ones(
-            (
-                height,
-                num_cols * width + topk * GRID_SPACING + Q_SPACING,
-                3
-            ),
-            dtype=np.uint8
-        )
-        grid_img[:, :width, :] = qimg
-
-        rank_idx = 1
-        for g_idx in indices[q_idx, :]:
-            gimg_paths, gpid, gcamid = gallery[g_idx]
-            gimg_path = gimg_paths[0]
-            invalid = (qpid == gpid) & (qcamid == gcamid)
-
-            if not invalid:
-                matched = gpid == qpid
-                border_color = GREEN if matched else RED
-                gimg = cv.imread(gimg_path)
-                gimg = cv.resize(gimg, (width, height))
-                gimg = cv.copyMakeBorder(
-                    gimg, BW, BW, BW, BW, cv.BORDER_CONSTANT, value=border_color
-                )
-                gimg = cv.resize(gimg, (width, height))
-                start = rank_idx * width + rank_idx * GRID_SPACING + Q_SPACING
-                end = (
-                              rank_idx + 1
-                      ) * width + rank_idx * GRID_SPACING + Q_SPACING
-                grid_img[:, start:end, :] = gimg
-
-                rank_idx += 1
-                if rank_idx > topk:
-                    break
-
-        imname = osp.basename(osp.splitext(qimg_path_name)[0])
-        cv.imwrite(osp.join(save_dir, imname + '.jpg'), grid_img)
-
-        if (q_idx + 1) % 100 == 0:
-            print('- done {}/{}'.format(q_idx + 1, num_q))
-
-    print('Done. Images have been saved to "{}" ...'.format(save_dir))
 
 
 class Visualizer:
@@ -140,6 +57,59 @@ class Visualizer:
             image = Visualizer.read_image(image_paths[0])
             Visualizer.save_image(image, out_image_path)
 
+    def visualize_ranked_results(self, q_image_tuple, g_image_tuples, width=128,
+                                 height=256, save_dir=''):
+        print('Visualizing top-{} ranks ...'.format(self.k))
+        if not osp.exists(save_dir):
+            os.makedirs(save_dir)
+
+        qimg_paths, qpid, qcamid = q_image_tuple
+        qimg_path = qimg_paths[0]
+
+        qimg = cv.imread(qimg_path)
+        qimg = cv.resize(qimg, (width, height))
+        qimg = cv.copyMakeBorder(
+            qimg, BW, BW, BW, BW, cv.BORDER_CONSTANT, value=(0, 0, 0)
+        )
+        # resize twice to ensure that the border width is consistent
+        qimg = cv.resize(qimg, (width, height))
+        num_cols = self.k + 1
+        grid_img = 255 * np.ones(
+            (height, num_cols * width + self.k * GRID_SPACING + Q_SPACING, 3),
+            dtype=np.uint8
+        )
+        grid_img[:, :width, :] = qimg
+
+        rank_idx = 1
+        for g_image_tuple in g_image_tuples:
+            gimg_paths, gpid, gcamid = g_image_tuple
+            gimg_path = gimg_paths[0]
+            invalid = (qpid == gpid) & (qcamid == gcamid)
+
+            if not invalid:
+                matched = gpid == qpid
+                border_color = GREEN if matched else RED
+                gimg = cv.imread(gimg_path)
+                gimg = cv.resize(gimg, (width, height))
+                gimg = cv.copyMakeBorder(
+                    gimg, BW, BW, BW, BW, cv.BORDER_CONSTANT,
+                    value=border_color
+                )
+                gimg = cv.resize(gimg, (width, height))
+                start = rank_idx * width + rank_idx * GRID_SPACING + Q_SPACING
+                end = (rank_idx + 1
+                       ) * width + rank_idx * GRID_SPACING + Q_SPACING
+                grid_img[:, start:end, :] = gimg
+
+                rank_idx += 1
+                if rank_idx > self.k:
+                    break
+
+            imname = osp.basename(osp.splitext(qimg_path)[0])
+            cv.imwrite(osp.join(save_dir, imname + '.jpg'), grid_img)
+
+        print('Done. Images have been saved to "{}" ...'.format(save_dir))
+
     def run(self, dist_mat, num=100):
         num_query, num_gallery = dist_mat.shape
 
@@ -168,5 +138,6 @@ class Visualizer:
             self.save_query_images(out_pid_dir, self.q_dataset[i])
             self.save_gallery_images(out_pid_dir, result_image_tuples,
                                      result_distances)
-            visualize_ranked_results(dist_mat, self.g_dataset, self.q_dataset,
-                                     save_dir=out_pid_dir)
+            self.visualize_ranked_results(self.q_dataset[i],
+                                          result_image_tuples,
+                                          save_dir=out_pid_dir)
