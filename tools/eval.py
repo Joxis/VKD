e8d90c05c95fb453c4fd5c68e246c757273a3cdd
Jules Talloen <jules.talloen@ml6.eu>
2020-09-30 15:00:49 +0200

    WIP

--- tools/eval.py
+++ tools/eval.py
@@ -240,7 +240,7 @@ def main():
 
     visualizer = Visualizer(gallery_loader.dataset.dataset,
                             query_loader.dataset.dataset)
-    visualizer.run(e.i2v_distmat)
+    visualizer.run(e.i2v_distmat, num=200)
 
 
 if __name__ == '__main__':
